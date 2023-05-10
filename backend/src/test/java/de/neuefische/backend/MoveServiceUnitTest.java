e21ffc6493dbd443e8f9aae7af45a06ff57e7cb3
Kathalania <katharina.stern@gmx.net>
2023-05-10 10:30:35 +0200

    Add Unit Test for getMoveById.

--- backend/src/test/java/de/neuefische/backend/MoveServiceUnitTest.java
+++ backend/src/test/java/de/neuefische/backend/MoveServiceUnitTest.java
@@ -3,17 +3,28 @@ package de.neuefische.backend;
 import de.neuefische.backend.move.Move;
 import de.neuefische.backend.move.MoveInterface;
 import de.neuefische.backend.move.MoveService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
+@SpringBootTest
 class MoveServiceUnitTest {
+
+
+    private final MoveInterface moveInterface = mock(MoveInterface.class);
+    private final MoveService moveService = new MoveService(moveInterface);
+
+    @DirtiesContext
     @Test
-    void addMove_ShouldReturnAddedMove_WhenMoveAdded(){
+    void addMove_ShouldReturnAddedMove_WhenMoveAdded() {
         //GIVEN
-        MoveInterface moveInterface = mock(MoveInterface.class);
-        MoveService moveService = new MoveService(moveInterface);
 
         Move tuckTurn = new Move("1", "Tuck Turn", "Rotational Rockstep, eindrehen, unter Arm durchdrehen über rechte Schulter", "Lindy Hop",
                 "6-count", "geschlossen", "offen");
@@ -28,7 +39,32 @@ class MoveServiceUnitTest {
 
     }
 
-    void getMoveById_ShouldReturnMoveWithGivenId(){
+    @DirtiesContext
+    @Test
+    void getMoveById_ShouldReturnMoveWithGivenId() {
+        //GIVEN
+        Move moveWithId = new Move("123", "Move", "", "", "", "", "");
+        when(moveInterface.findById("123")).thenReturn(Optional.of(moveWithId));
 
+        //WHEN
+        Move actual = moveService.getMoveById("123");
+
+        //THEN
+        Move expected = new Move("123", "Move", "", "", "", "", "");
+        verify(moveInterface).findById("123");
+        assertEquals(actual, expected);
     }
+
+    @Test
+    @DirtiesContext
+    void getMoveById_ShouldReturnException_WhenIdNotFound() {
+        //GIVEN
+        when(moveInterface.findById("2")).thenThrow(NoSuchElementException.class);
+
+        //WHEN
+
+        Assertions.assertThrows(NoSuchElementException.class, () -> moveService.getMoveById("2"));
+        verify(moveInterface).findById("2");
+    }
+
 }
